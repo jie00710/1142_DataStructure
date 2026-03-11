b2652eedf566ad51d9d53acbe74c5811e1f3113f
jie00710 <157110312+jie00710@users.noreply.github.com>
2026-03-11 23:25:13 +0800

    Implement countElement function for vector element counting
    Added a function to count occurrences of a target element in a vector and track step counts for operations.

--- Lab02/Lab02_Q3_2.cpp
+++ Lab02/Lab02_Q3_2.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include<vector>
+using namespace std;
 
+int stepCount = 0;
+
+int countElement(const vector<int>& arr, int target) {
+  int count = 0;
+  stepCount++;//assignments:1
+
+  stepCount++;//loop initialization:1
+  for (int i = 0; i < arr.size(); i++) {
+    stepCount++// Comparison : i < arr:n+1
+  
+    stepCount++// Comparison with target: arr[i] == target:n
+    if (arr[i] == target) {
+      count++;
+      stepCount++//assignments:n
+  }
+    stepCount++// Increment-i++:n
+  }
+  stepCount++//return count
+  return count;
+
+}
+
+//time complexity
+// Total operations:
+//  4 + 4n operations
+// Therefore, O(n) complexity
