5f6406f5d6b8d60142ef7ab1a2a28ebf430bb52f
jie00710 <157110312+jie00710@users.noreply.github.com>
2026-03-11 23:25:01 +0800

    Update Lab02_Q3_1.cpp

--- Lab02/Lab02_Q3_1.cpp
+++ Lab02/Lab02_Q3_1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int count = 0 ;
+void copyArray(const vector<int>& source, vector<int>& dest) {
+    count++// loop initialization:1
+  for (int i = 0; i < source.size(); i++) {
+    count++//Loop comparison:n+1
+    dest[i] = source[i];
+    count++//assignment:n
+    
+    count ++; // Increment-i++:n
+}
+}
 
+//time complexity is 3n+2
+// Total operations:
+//  2 + 3n operations
+// Therefore, O(n) complexity
